6df284528436b2ef2fb04f17db12ba6276df57f6
Cem DURAK <cem@admongrel.com>
2020-08-11 22:02:11 +0300

    Mentor update
    - In situations where you are looking something up quickly, based a value, and then getting another value, one of the best tools you can use is a map.(https://blog.golang.org/maps) This provides a significantly faster lookup than iterating over an array to find a given value.
    
    - When you want to check if a key exists in a map, it's not safe to rely on the zero value. The zero value is a often a valid value, so checking for it isn't reliable. Instead, use two-value assignment (https://tour.golang.org/moretypes/22) to safely check if a key exists in a map.

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -4,33 +4,30 @@ package space
 // Planet is type for planet
 type Planet string
 
-var periods = []struct {
-	planet Planet
-	period float64
-}{
-	{"Mercury", 0.2408467},
-	{"Venus", 0.61519726},
-	{"Earth", 1},
-	{"Mars", 1.8808158},
-	{"Jupiter", 11.862615},
-	{"Saturn", 29.447498},
-	{"Uranus", 84.016846},
-	{"Neptune", 164.79132},
+var periods = map[Planet]float64{
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Earth":   1,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
 }
 
 const earthYearInSec float64 = 31557600
 
 // Age calculates age in Earth-years
 func Age(ageInSec float64, planet Planet) float64 {
-	var ageOnPlanet float64
+	ageOnEarth := ageInSec / earthYearInSec
 
-	for _, p := range periods {
-		if p.planet == planet {
-			ageOnEarth := ageInSec / earthYearInSec
-			ageOnPlanet = ageOnEarth / p.period
-			break
-		}
+	orbitalPeriod, ok := periods[planet]
+
+	if !ok || orbitalPeriod == 0 {
+		return -1
 	}
+
+	ageOnPlanet := ageOnEarth / orbitalPeriod
 
 	return ageOnPlanet
 }
